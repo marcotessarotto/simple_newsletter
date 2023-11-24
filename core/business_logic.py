8d42a08f123e577d77ee79606fe3d4ed71ef7532
marco <marcotts@gmail.com>
2023-11-24 11:49:48 +0100

    update

---
+++ core/business_logic.py
@@ -0,0 +1,26 @@
+from django.utils import timezone
+from .models import EventLog
+
+
+def create_event_log(event_type, event_title, event_data, event_target=None):
+    """
+    Creates an instance of EventLog with the provided details.
+
+    Args:
+    event_type (str): The type of the event (e.g., EMAIL_SENT, NEWSLETTER_SUBSCRIPTION_CONFIRMED).
+    event_title (str): A short title or description of the event.
+    event_data (str): Additional data or details about the event.
+    event_target (str, optional): The target or subject of the event. Defaults to None.
+
+    Returns:
+    EventLog: The created EventLog instance.
+    """
+    event_log = EventLog(
+        event_type=event_type,
+        event_title=event_title,
+        event_data=event_data,
+        event_target=event_target,
+        created_at=timezone.now()
+    )
+    event_log.save()
+    return event_log
--- core/management/commands/SendEmailToVisitors.py
+++ core/management/commands/SendEmailToVisitors.py
@@ -1,6 +1,9 @@
 from django.core.management import BaseCommand
+from django.utils import timezone
 
-from core.models import Visitor
+from core.business_logic import create_event_log
+from core.models import Visitor, EmailTemplate
+from core.template_utils import render_template_from_string
 
 
 class Command(BaseCommand):
@@ -9,19 +12,50 @@ class Command(BaseCommand):
     to sign up for the newsletter.
     """
     def add_arguments(self, parser):
-        pass
+        parser.add_argument("sender_email", type=str)
+        parser.add_argument("--template", type=str, default=None)
 
-    def handle(self, *args, **options):
+    def handle(self, send_custom_email_task=None, *args, **options):
+        sender_email = options["sender_email"]
+        template = options.get("template")
 
+        instance = EmailTemplate.objects.get(name=template)
+        subject = instance.subject
+        template_content = instance.body
+
+        html_content = render_template_from_string(template_content, context={})
         # get all Visitors
 
         rs = Visitor.objects.filter(email_address__isnull=False).filter(email_sent=False)
 
+        counter = 0
+
         # for each visitor, send an email with a link to the questionnaire
         for visitor in rs:
+            print(visitor.email_address)
 
-            # subject = 'Welcome to the Digital Culture Lab!'
+            send_custom_email_task.delay(
+                sender_email,
+                visitor.email_address,
+                subject,
+                html_content,
+                # bcc=['']
+            )
 
-            # message = f'Dear {visitor.first_name} {visitor.last_name},\n\n' \
+            visitor.email_sent = True
+            visitor.email_sent_at = timezone.now()
+            visitor.save()
 
-            pass
+            create_event_log(
+                event_type="EMAIL_SENT",
+                event_title=f"Email sent to visitor - subject: {subject}",
+                event_data=f"Visitor: {visitor.email_address}",
+                event_target=visitor.email_address
+            )
+
+            counter += 1
+            if counter % 5 == 0:
+                break
+
+        print(f"Sent {counter} emails")
+
