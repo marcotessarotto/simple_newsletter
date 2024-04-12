c07193a07db975f81d88b1767e77c5d973c7c511
marco <marcotts@gmail.com>
2024-04-12 12:27:55 +0200

    update

--- core/serializers.py
+++ core/serializers.py
@@ -1,42 +1,30 @@
 from rest_framework import serializers
-from .models import SubscriptionToNewsletter
+from .models import SubscriptionToNewsletter, Newsletter
 
 
 class SubscriptionToNewsletterSerializer(serializers.ModelSerializer):
+    newsletter_id = serializers.PrimaryKeyRelatedField(
+        source='newsletter',  # Map newsletter_id to the newsletter ForeignKey
+        queryset=Newsletter.objects.all(),
+        write_only=True  # Only used for incoming data, not serialized in responses
+    )
+
     class Meta:
         model = SubscriptionToNewsletter
-        fields = '__all__'  # Or specify the fields you want to expose
-        fields = ['honorific',
-                  'email',
-                  'name',
-                  'surname',
-                  'nationality',
-                  'company',
-                  'role',
-                  'telephone',
-                  'ip_address',
-                  'subscription_source',
-                  'privacy_policy_accepted',
-                  'subscribed',
-                  'subscription_confirmed',
-                  'newsletter_id']
+        fields = [
+            'newsletter_id', 'honorific', 'email', 'name', 'surname', 'nationality',
+            'company', 'role', 'telephone', 'ip_address', 'subscription_source',
+            'privacy_policy_accepted', 'subscribed', 'subscription_confirmed'
+        ]
 
     def validate_email(self, value):
-        """
-        Check that the email does not already exist for an active subscription.
-        """
-
-        newsletter_id = self.initial_data.get('newsletter_id', None)
-
-        if SubscriptionToNewsletter.objects.filter(email=value, subscribed=True, newsletter_id=newsletter_id).exists():
-            raise serializers.ValidationError("A subscription with this email already exists.")
+        # Validation to ensure email does not exist for an active subscription with the same newsletter
+        if self.initial_data.get('newsletter_id') and SubscriptionToNewsletter.objects.filter(
+                email=value, subscribed=True, newsletter_id=self.initial_data.get('newsletter_id')).exists():
+            raise serializers.ValidationError("A subscription with this email already exists for this newsletter.")
         return value
 
     def validate_privacy_policy_accepted(self, value):
-        """
-        Check that the privacy policy has been accepted.
-        """
-
         if not value:
             raise serializers.ValidationError("You must accept the privacy policy.")
         return value
